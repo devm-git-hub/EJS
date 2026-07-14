f5a80b6c458e64a19d364377702fc24437620928
Dev Mandal <devmandalgps@gmail.com>
2026-07-14 21:24:42 +0530

    Passing data to EJS

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render("home.ejs");
 })
+
+
+//Passing Data to the ejs file
+app.get('/rollDice', (req, res) => {
+    let num = Math.floor(Math.random() * 6) + 1;
+    res.render("rollDice.ejs", { num });
+});
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
