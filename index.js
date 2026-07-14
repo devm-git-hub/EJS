cfeb411a439adc20af1097771de4b3d3ef54bb0d
Dev Mandal <devmandalgps@gmail.com>
2026-07-14 10:22:23 +0530

    Using ejs

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 let port = 9000;
+
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render("home.ejs");
+})
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
